ef5ad20e6741a8d8ad3d9f3660a66a7301dc277a
Miguel Javier Dev <125704220+MJavDev@users.noreply.github.com>
2023-09-06 15:03:22 -0300

    src

---
+++ src/Components/ItemList/ItemList.jsx
@@ -0,0 +1,16 @@
+import ItemCard from "../ItemCard/ItemCard";
+import "./ItemList.scss";
+
+const ItemList = ({ productos }) => {
+  return (
+    <div className="contenedor">
+      <div className="grid">
+        {productos.map((prod) => (
+          <ItemCard key={prod.id} item={prod} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ItemList;
