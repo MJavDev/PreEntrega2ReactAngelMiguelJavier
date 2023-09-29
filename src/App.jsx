7b1103d4e0a82cdfef4057a5c4040334f38defa0
Miguel Javier Dev <m.javierdev@gmail.com>
2023-09-29 13:22:01 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,63 @@
-import "./index.css";
-import "./App.css";
-import { Carrousel } from "./components/Carrousel/Carrousel";
-import { Header } from "./components/Header/Header";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import './index.css'
+import './App.css'
+import { Header } from "./components/Header/Header"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
-import PortadaCat from "./components/PortadaCat/PortadaCat";
-import { Categories } from "./components/Categories/Categories";
-import { Footer } from "./components/Footer/Footer";
-import { Contact } from "./components/Contact/Contact";
-import { Store } from "./components/Store/Store";
-import { About } from "./components/About/About";
-import { Delivery } from "./components/Delivery/Delivery";
-import { Guarantee } from "./components/Guarantee/Guarantee";
-import { Why } from "./components/WhyPlantopia/WhyPlantopia";
-import { PageNotFound } from "./components/PageNotFound/PageNotFound";
+import { Footer } from "./components/Footer/Footer"
+import { PageNotFound } from './components/PageNotFound/PageNotFound'
+import { CartProvider } from "./context/CartContext";
+import WishView from "./components/WishView/WishView";
+import CartView from "./components/CartView/CartView";
+import { SearchProvider } from './context/SearchContext'
+import Checkout from "./components/Checkout/Checkout";
+import { WishProvider } from './context/WishContext'
+import { ContactRoute } from './components/ContactRoute/ContactRoute'
+import { AboutRoute } from './components/AboutRoute/AboutRoute'
+import { HomeRoute } from './components/HomeRoute/HomeRoute'
+import { CategoryRoute } from './components/CategoryRoute/CategoryRoute'
+
+
+
 
 function App() {
+
+  
   return (
-    <div>
-      <BrowserRouter>
-        <Header />
+    <div>        
+          <CartProvider>
+            <WishProvider>
+              <BrowserRouter>
+              
+                <SearchProvider>
+                  <Header />        
+                  
+                  <Routes>
+                    <Route path="/" element={ <HomeRoute/> }/>
+                    
+                    <Route path="/products/:categoryId" element={ <CategoryRoute/> }/>
+                    
+                    <Route path="/detail/:itemId" element={ <ItemDetailContainer /> }/>                    
+                    
+                    <Route path="/wish" element={ <WishView /> }/>
 
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <Carrousel />
-                <Categories />
-                <ItemListContainer />
-                <Delivery />
-                <Guarantee />
-              </div>
-            }
-          />
+                    <Route path="/cart" element={ <CartView /> }/>
+                    
+                    <Route path="/checkout" element={ <Checkout/> }/>
+                  
+                    <Route path="/contact" element={ <ContactRoute/> }/>                    
 
-          <Route
-            path="/products/:categoryId"
-            element={
-              <div>
-                <PortadaCat />
-                <Categories />
-                <ItemListContainer />
-                <Delivery />
-                <Guarantee />
-              </div>
-            }
-          />
-          <Route path="/detail/:itemId" element={<ItemDetailContainer />} />
+                    <Route path="/about" element={ <AboutRoute/> }/>
 
-          <Route
-            path="/contact"
-            element={
-              <div>
-                <Store />
-                <Contact />
-              </div>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <div>
-                <About />
-                <Why />
-              </div>
-            }
-          />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+                    <Route path="/*" element={ <PageNotFound/> }/>          
+                  </Routes>
+                  <Footer/>  
+                </SearchProvider>
+
+              </BrowserRouter>
+            </WishProvider>
+          </CartProvider>        
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
